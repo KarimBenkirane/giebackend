87a645bcd44a9394cd2855671c7d4bbc351b31d8
Karim <benkiranemedkarim@gmail.com>
2024-03-28 23:51:18 +0000

    Ajout Particulier opérationnelle, Ajout Entreprise à faire

--- src/main/java/org/uiass/eia/controller/ContactController.java
+++ src/main/java/org/uiass/eia/controller/ContactController.java
@@ -136,46 +136,46 @@ public class ContactController {
 
 
 
-            if(!(jsonEmail instanceof JsonNull)) {
+            if(!(jsonEmail == null)) {
                 email = jsonEmail.getAsString();
             }
-            if(!(jsonFax instanceof JsonNull)) {
+            if(!(jsonFax == null)) {
                 fax = jsonFax.getAsString();
             }
-            if(!(jsonTelephone instanceof JsonNull)) {
+            if(!(jsonTelephone == null)) {
                 telephone = jsonTelephone.getAsString();
             }
-            if(!(jsonNom instanceof JsonNull)) {
+            if(!(jsonNom == null)) {
                 nom = jsonNom.getAsString();
             }
-            if(!(jsonPrenom instanceof JsonNull)) {
+            if(!(jsonPrenom == null)) {
                 prenom = jsonPrenom.getAsString();
             }
 
 
-            if(!(jsonRue instanceof JsonNull)) {
+            if(!(jsonRue == null)) {
                 rue = jsonRue.getAsString();
             }
-            if(!(jsonNumeroRue instanceof JsonNull)) {
+            if(!(jsonNumeroRue == null)) {
                 numeroRue = jsonNumeroRue.getAsInt();
             }
-            if(!(jsonCodePostal instanceof JsonNull)) {
+            if(!(jsonCodePostal == null)) {
                 codePostal = jsonCodePostal.getAsInt();
             }
-            if(!(jsonQuartier instanceof JsonNull)) {
+            if(!(jsonQuartier == null)) {
                 quartier = jsonQuartier.getAsString();
             }
-            if(!(jsonVille instanceof JsonNull)) {
+            if(!(jsonVille == null)) {
                 ville = jsonVille.getAsString();
             }
-            if(!(jsonPays instanceof JsonNull)) {
+            if(!(jsonPays == null)) {
                 pays = jsonPays.getAsString();
             }
 
 
             Adresse adresseObjet = new Adresse(rue,numeroRue,quartier,codePostal,ville,pays);
             contactController.adresseDao.addAdresse(adresseObjet);
-            contactController.contactDao.addParticulier(telephone,email,fax,adresseObjet,nom,prenom);
+            contactController.contactDao.addParticulier(new Particulier(telephone,email,fax,adresseObjet,nom,prenom));
 
             return "Particulier ajouté avec succès!";
 
